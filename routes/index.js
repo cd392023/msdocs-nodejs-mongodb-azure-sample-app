d2fd42197f5fec881045f3d5cb2ba6858beac185
Csaba Dudas <csaba.dudas@kyndryl.com>
2023-04-09 19:43:53 +0000

    console.log key vaul reference

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res, next) {
       const completedTasks = tasks.filter(task => task.completed === true);
 
       console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
-      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks, message: process.env.message });
+      console.log(process.env.message)
+      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks});
     })
     .catch((err) => {
       console.log(err);
